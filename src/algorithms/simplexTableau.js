669f7d62678508d758452102b5e4bcafd49e3d8b
Riccardo Locci <r.locci@beetobit.com>
2020-09-19 14:44:19 +0200

    DS-12: Refactoring and new support functions

--- src/algorithms/simplexTableau.js
+++ src/algorithms/simplexTableau.js
@@ -1,72 +1,98 @@
+import { getPoint, swapColumns } from '../utils';
+
 const numbers = require('numbers');
 const _ = require('lodash');
 
 
-export const findPivot = (bOverbar, FOverbar, indexH) => {
-    let thisIndexT = null, minValue = null;
-    let thisColH = numbers.matrix.getCol(FOverbar, indexH);
+export const dualFindPivot = (cPrimeBarF, FOverbar, indexT) => {
+    let indexH = null, minValue = null;
+    let rowT = FOverbar[indexT];
 
-    bOverbar.forEach((el, idx) => {
-        console.log('el', el);
-        console.log('thisColH[idx]', thisColH[idx]);
+    cPrimeBarF.forEach((el, idx) => {
+        if(rowT[idx] < 0){
+            let valueIndexT = el[0] / rowT[idx];
 
-        if(thisColH[idx] > 0){
-            let value_indexH = el[0] / thisColH[idx];
-            console.log('value_indexH', value_indexH);
-
-            if(value_indexH < minValue || minValue === null){
-                minValue = value_indexH;
-                thisIndexT = idx;
+            if(valueIndexT < minValue || minValue === null){
+                minValue = valueIndexT;
+                indexH = idx;
             }
         }
     });
 
     return {
-        thisColH, thisIndexT
+        rowT, indexH, showPivot: true
     }
 }
 
-export const init = (A, b, c_prime) => {
+export const dualOptimalityTest = (bOverbar) => {
+    for(let i=0; i < bOverbar.length; i++) if(bOverbar[i][0] < 0) return i;
+    return null
+}
+
+export const dualUpdateTableau = (prevTableau) => {}
+
+export const findPivot = (bOverbar, FOverbar, indexH) => {
+    let indexT = null, minValue = null;
+    let colH = numbers.matrix.getCol(FOverbar, indexH);
+
+    bOverbar.forEach((el, idx) => {
+        if(colH[idx] > 0){
+            let valueIndexH = el[0] / colH[idx];
+
+            if(valueIndexH < minValue || minValue === null){
+                minValue = valueIndexH;
+                indexT = idx;
+            }
+        }
+    });
+
+    return {
+        colH, indexT, showPivot: true
+    }
+}
+
+export const init = (A, b, cPrime) => {
     let B = numbers.matrix.identity(A.length)
 
     let xFLables = _.range(1, A.length+1).map(n => `x${n}`);
     let xBLables = _.range(xFLables.length+1, xFLables.length+A.length+1).map(n => `x${n}`);
 
-    let B_inv = numbers.matrix.inverse(B);
+    let BInv = numbers.matrix.inverse(B);
     let F = A.slice();
 
-    let BInvB = numbers.matrix.multiply(B_inv, B);
+    let BInvB = numbers.matrix.multiply(BInv, B);
 
     //TRASPOSTO PERCHE' DAL FILE LO STO PASSANDO COME UN VETTORE RIGA, MA DEVE ESSERE UN VETTORE COLONNA
-    let b_prime = numbers.matrix.transpose([b]);
+    let bPrime = numbers.matrix.transpose([b]);
     
-    let bOverbar = numbers.matrix.multiply(B_inv, b_prime);
-    let FOverbar =  numbers.matrix.multiply(B_inv, F);
+    let bOverbar = numbers.matrix.multiply(BInv, bPrime);
+    let FOverbar =  numbers.matrix.multiply(BInv, F);
 
     //VETTORE DEI COSTI DELLE VARIABILI IN BASE: NULLO ALL'INIZIO PERCHE' HO LE SLACK IN BASE
-    let c_prime_B = numbers.matrix.zeros(1, B.length);
+    let cPrimeB = numbers.matrix.zeros(1, B.length);
 
     //VETTORE DEI COSTI DELLE VARIABILI FUORI BASE
-    let c_prime_F = [c_prime.slice()];
+    let cPrimeF = [cPrime.slice()];
 
-    let c_prime_B_bOverbar = numbers.matrix.multiply(c_prime_B, bOverbar);
-    let cBar0 = -c_prime_B_bOverbar[0][0];
+    let cPrimeBbOverbar = numbers.matrix.multiply(cPrimeB, bOverbar);
+    let cBar0 = -cPrimeBbOverbar[0][0];
 
     //VETTORE DEI COSTI RIDOTTI DELLE VARIABILI IN BASE: NULLO ALL'INIZIO PERCHE' HO LE SLACK IN BASE
     let cPrimeBarB = numbers.matrix.zeros(1, B[0].length)
 
     //VETTORE DEI COSTI RIDOTTI DELLE VARIABILI FUORI BASE
-    let c_prime_B_FOverbar = numbers.matrix.multiply(c_prime_B, FOverbar);
-    let cPrimeBarF = numbers.matrix.subtraction(c_prime_F, c_prime_B_FOverbar);
+    let cPrimeBFOverbar = numbers.matrix.multiply(cPrimeB, FOverbar);
+    let cPrimeBarF = numbers.matrix.subtraction(cPrimeF, cPrimeBFOverbar);
+
+    let point = getPoint(xBLables, bOverbar);
 
     return {
-        cBar0, xBLables, xFLables, cPrimeBarB, cPrimeBarF, bOverbar, BInvB, FOverbar
+        cBar0, xBLables, xFLables, cPrimeBarB, cPrimeBarF, bOverbar, BInvB, FOverbar, point
     }
 }
 
-export const optimalityTest = c_prime_F => { 
-    console.log('c_prime_F', c_prime_F)
-    for(let i=0; i < c_prime_F.length; i++) if(c_prime_F[i] < 0) return i;
+export const optimalityTest = cPrimeF => {
+    for(let i=0; i < cPrimeF.length; i++) if(cPrimeF[i] < 0) return i;
     return null
 }
 
@@ -77,4 +103,46 @@ export const printTableau = (cBar0, xBLables, xFLables, cPrimeBarB, cPrimeBarF, 
         console.log(xBLables[l], bOverbar[l][0], JSON.stringify(BInvB[l]), JSON.stringify(FOverbar[l]));
     }
     console.log('\n');
+}
+
+export const updateTableau = (prevTableau) => {
+    let { cBar0, bOverbar, BInvB, FOverbar, cPrimeBarB, cPrimeBarF, xBLables, xFLables } = prevTableau;
+    const { colH, indexH, indexT } = prevTableau;
+
+    prevTableau.showPivot = false;
+
+    let aTH = colH[indexT];
+
+    //UPDATING TABLES
+    let zPivot = cPrimeBarF[0][indexH];
+
+    bOverbar = numbers.matrix.rowScale(bOverbar, indexT, 1/aTH);
+    BInvB = numbers.matrix.rowScale(BInvB, indexT, 1/aTH);
+    FOverbar = numbers.matrix.rowScale(FOverbar, indexT, 1/aTH);
+
+    cBar0 -= zPivot*bOverbar[indexT];
+
+    cPrimeBarB = numbers.matrix.subtraction(cPrimeBarB, [numbers.matrix.rowScale(BInvB, indexT, zPivot)[indexT]]);
+    cPrimeBarF = numbers.matrix.subtraction(cPrimeBarF, [numbers.matrix.rowScale(FOverbar, indexT, zPivot)[indexT]]);
+    
+    for(let l=0; l<bOverbar.length; l++){
+        if(l === indexT) continue;
+
+        bOverbar = numbers.matrix.rowAddMultiple(bOverbar, indexT, l, -colH[l]);
+        BInvB = numbers.matrix.rowAddMultiple(BInvB, indexT, l, -colH[l]);
+        FOverbar = numbers.matrix.rowAddMultiple(FOverbar, indexT, l, -colH[l]);
+    }
+    
+    [BInvB, FOverbar] = swapColumns(BInvB, indexT, FOverbar, indexH);
+    [cPrimeBarB, cPrimeBarF] = swapColumns(cPrimeBarB, indexT, cPrimeBarF, indexH);
+
+    [xBLables[indexT], xFLables[indexH]] = [xFLables[indexH], xBLables[indexT]];
+
+    return {
+        ...prevTableau, 
+        ...{
+            cBar0, bOverbar, BInvB, FOverbar, cPrimeBarB, cPrimeBarF, 
+            point: getPoint(xBLables, bOverbar), xBLables, xFLables
+        }
+    }
 }
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,13 @@
+export const getPoint = (xBLables, bOverbar) => {
+    let x1Idx = xBLables.indexOf('x1');
+    let x2Idx = xBLables.indexOf('x2');
+
+    let x1 = x1Idx >= 0 ? bOverbar[x1Idx][0] : 0;
+    let x2 = x2Idx >= 0 ? bOverbar[x2Idx][0] : 0;
+
+    return [x1, x2]
+}
+
 export const getY = (line, x, max=10000) => {
     let r = -(line[0]*(x) + line[2])/line[1];
 
@@ -10,4 +20,12 @@ export const getX = (line, y, max=10000) => {
     return isFinite(r) ? r : r > 0 ? max : -max;
 }
 
-export default {getX, getY};
+export const swapColumns = (B, indexT, F, indexH) => {
+    B.forEach((el, i) => {
+        [el[indexT], F[i][indexH]] = [F[i][indexH], el[indexT]]
+    })
+
+    return [B, F]
+}
+
+export default {getX, getY, swapColumns};
