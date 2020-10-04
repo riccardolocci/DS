84e87e65a20f6a271f60dae05c560cfe2f8052a8
Riccardo Locci <riccardo.locci.95@gmail.com>
2020-10-04 21:13:15 +0200

    DS-4: Added random generation and download button

--- src/components/RandomManager.js
+++ src/components/RandomManager.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Button, CircularProgress, TextField } from '@material-ui/core';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
 
-// import generate from '../utilities/randomGenerator';
+import { generate } from '../utils';
 
 const useStyles = makeStyles((theme) => createStyles({
     input: {
@@ -15,21 +15,18 @@ const useStyles = makeStyles((theme) => createStyles({
 let RandomManager = (props) => {
     const classes = useStyles();
 
-    let [ n, setN ] = useState(2);
-    let [ m, setM ] = useState(1);
+    let [ m, setM ] = useState(2);
 
-    let nMax = 50;
-    let mMax = n*(n-1)/2;
+    let mMax = 50;
     
     return (
     <>
-        Otherwise, you can generate a random graph:
+        Otherwise, you can generate a random linear problem:
         <br/>
         
-        <TextField className={classes.input} label='Nodes' type="number" inputProps={{ min: 1, max: nMax, step: 1 }} value={n} onChange={(e) => {let v = e.target.value; setN(v > nMax ? nMax : v); if(m > v*(v-1)/2) setM(v*(v-1)/2)}}/>
-        <TextField className={classes.input} label='Edges' type="number" inputProps={{ min: 0, max: mMax, step: 1 }} value={m} onChange={(e) => setM(e.target.value > mMax ? mMax : e.target.value)} disabled={!n}/>
+        <TextField className={classes.input} label='Constraints' type="number" inputProps={{ min: 1, max: mMax, step: 1 }} value={m} onChange={(e) => {let v = e.target.value; setM(v > mMax ? mMax : v)}}/>
         <br/>
-        <Button disabled={!n || !m} variant='outlined' onClick={() => {} /*props.getFile(generate(n, m))*/}>{props.loading ? (<CircularProgress/>) : 'GENERATE'}</Button>
+        <Button disabled={!m} variant='outlined' onClick={() => props.getFile(generate(m))}>{props.loading ? (<CircularProgress/>) : 'GENERATE'}</Button>
         
     </>
 )}
